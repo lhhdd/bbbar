967bcba77f7ec36acd905ee6291b4bdd6ffe3579
lh <1037169827@qq.com>
2018-11-21 15:16:49 +0800

    修改

---
+++ git/git_test/src/com/itcast/gitTest.java
@@ -0,0 +1,18 @@
+package com.itcast;
+
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+        System.out.println("hello word");
+        System.out.println("hello word");
+        System.out.println("hello word");
+
+
+
+        int a = 10;
+        int b = 20;
+        int c;
+        c = a + b;
+        System.out.println(c);
+    }
+}
